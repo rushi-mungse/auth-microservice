cb13fc94b1add3ac48dfdeaf3afc345057f086e5
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2023-12-21 21:34:14 +0530

    add: config file for env varibles

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,14 +1,21 @@
-import dotenv from "dotenv";
-dotenv.config();
+import { config } from "dotenv";
+import path from "path";
+
+config({
+    path: path.join(__dirname, `../../.env.${process.env.NODE_ENV || "dev"}`),
+});
 
 export const {
     PORT,
     NODE_ENV,
+    HASH_SECRET,
     DB_HOST,
     DB_USERNAME,
     DB_PASSWORD,
     DB_PORT,
     DB_NAME,
+    PRIVATE_KEY,
+    REFRESH_TOKEN_SECRET,
 } = process.env;
 
 export { default as logger } from "./logger";
