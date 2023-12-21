ca171af30a4e116fb6182c967f06b1834030d3e0
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2023-12-21 21:36:08 +0530

    add: auth routes

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,11 +1,43 @@
-import express from "express";
+import express, {
+    NextFunction,
+    Request,
+    RequestHandler,
+    Response,
+} from "express";
 import { AuthController } from "../controllers";
+import { sendOtpDataValidator, verifyOtpDataValidator } from "../validators";
+import { AppDataSource, logger } from "../config";
+import { Token, User } from "../entity";
+import { CredentialService, TokenService, UserService } from "../services";
 const router = express.Router();
 
-const authController = new AuthController();
+/* Dependancy Injection */
+const userRepository = AppDataSource.getRepository(User);
+const tokenRepository = AppDataSource.getRepository(Token);
 
-router.post("/register/send-otp", (req, res, next) =>
-    authController.sendOtp(req, res, next),
+const userService = new UserService(userRepository);
+const tokenService = new TokenService(tokenRepository);
+
+const credentialService = new CredentialService();
+const authController = new AuthController(
+    userService,
+    logger,
+    credentialService,
+    tokenService,
+);
+
+router.post(
+    "/register/send-otp",
+    sendOtpDataValidator,
+    (req: Request, res: Response, next: NextFunction) =>
+        authController.sendOtp(req, res, next) as unknown as RequestHandler,
+);
+
+router.post(
+    "/register/verify-otp",
+    verifyOtpDataValidator,
+    (req: Request, res: Response, next: NextFunction) =>
+        authController.verifyOtp(req, res, next) as unknown as RequestHandler,
 );
 
 export default router;
