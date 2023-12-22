dd8fd6ec43639ec42a126fc758baab72a9456a70
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2023-12-23 05:25:11 +0530

    add: check access token middleware

---
+++ src/middlewares/access.middleware.ts
@@ -0,0 +1,23 @@
+import { Request, RequestHandler } from "express";
+import { GetVerificationKey, expressjwt } from "express-jwt";
+import jwksClient from "jwks-rsa";
+import { IAuthCookie } from "../types";
+import { JWKS_URI } from "../config";
+
+export default expressjwt({
+    secret: jwksClient.expressJwtSecret({
+        jwksUri: JWKS_URI!,
+        cache: true,
+        rateLimit: true,
+    }) as GetVerificationKey,
+    algorithms: ["RS256"],
+    getToken(req: Request) {
+        const authHeader = req.headers.authorization;
+        if (authHeader && authHeader.split(" ")[1] !== "undefined") {
+            const accessToken = authHeader.split(" ")[1];
+            if (accessToken) return accessToken;
+        }
+        const { accessToken } = req.cookies as IAuthCookie;
+        return accessToken;
+    },
+});
