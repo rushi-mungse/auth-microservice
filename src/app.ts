a28d14dc10d8089c2c15148227e074a1d7fc8e2a
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2023-12-23 05:23:53 +0530

    add: cookie parser

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,11 @@ import "reflect-metadata";
 import express from "express";
 import { errorHandlerMiddleware } from "./middlewares";
 import { authRouter } from "./routes";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
+app.use(cookieParser());
 app.use(express.static("public"));
 app.use(express.json());
 
