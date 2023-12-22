dace83b0dbf64c5a67e90cce3f1a908cfb9d8505
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2023-12-23 05:25:50 +0530

    add: check refresh token

---
+++ src/middlewares/refresh.middleware.ts
@@ -0,0 +1,34 @@
+import { Request } from "express";
+import { expressjwt } from "express-jwt";
+import { IAuthCookie, TPayload } from "../types";
+import { AppDataSource, logger, REFRESH_TOKEN_SECRET } from "../config";
+import { Token } from "../entity";
+
+/* validate refresh token and check if revoked refresh token */
+export default expressjwt({
+    secret: REFRESH_TOKEN_SECRET!,
+    algorithms: ["HS256"],
+    getToken(req: Request) {
+        const { refreshToken } = req.cookies as IAuthCookie;
+        return refreshToken;
+    },
+    async isRevoked(req: Request, token) {
+        const payload = token?.payload as TPayload;
+        try {
+            /* check refresh token is in db */
+            const tokenRepository = AppDataSource.getRepository(Token);
+            const tokenRef = await tokenRepository.findOne({
+                where: {
+                    id: Number(payload.tokenId),
+                    user: { id: Number(payload.userId) },
+                },
+            });
+            return tokenRef === null;
+        } catch (error) {
+            logger.error("Error while getting the refresh token", {
+                userId: payload.userId,
+            });
+        }
+        return true;
+    },
+});
