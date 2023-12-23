d11fabd472fd385e5ccc21ed9845ba997330befc
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2023-12-23 06:31:21 +0530

    add: login data validator

--- src/validators/index.ts
+++ src/validators/index.ts
@@ -1,2 +1,3 @@
 export { default as sendOtpDataValidator } from "./sendOtpData.validator";
 export { default as verifyOtpDataValidator } from "./verifyOtpData.validator";
+export { default as loginDataValidator } from "./loginData.validator";
