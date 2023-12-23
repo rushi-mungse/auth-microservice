6479adbcdd10e029e16a6363b4ad5a59efeb1589
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2023-12-23 11:38:12 +0530

    add: forget password data and set password data validatior

--- src/validators/index.ts
+++ src/validators/index.ts
@@ -1,3 +1,5 @@
 export { default as sendOtpDataValidator } from "./sendOtpData.validator";
 export { default as verifyOtpDataValidator } from "./verifyOtpData.validator";
 export { default as loginDataValidator } from "./loginData.validator";
+export { default as forgetPasswordDataValidator } from "./forgetPasswordData.validator";
+export { default as setPasswordDataValidator } from "./setPasswordData.validator";
