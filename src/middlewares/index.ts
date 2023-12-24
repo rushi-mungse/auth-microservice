000999164a192dd2d95235e02881db29203514e1
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2023-12-24 11:47:28 +0530

    add: permission middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -2,3 +2,4 @@ export { default as errorHandlerMiddleware } from "./error.middleware";
 export { default as checkAccessToken } from "./access.middleware";
 export { default as checkRefreshToken } from "./refresh.middleware";
 export { default as isInvalidRefreshToken } from "./isInvalidRefreshToken.middleware";
+export { default as permissionMiddleware } from "./permission.middleware";
