21b3a42b23f32892a28c63a7a8c6f32e5bda152f
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2023-12-29 11:08:51 +0530

    add: delete my self and update fullName methods

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response, NextFunction } from "express";
 import { UserService } from "../services";
 import createHttpError from "http-errors";
+import { IAuthRequest, IUpdateFullNameRequest } from "../types";
+import { validationResult } from "express-validator";
 
 class UserController {
     constructor(private userService: UserService) {}
@@ -39,6 +41,54 @@ class UserController {
             if (!user) return next(createHttpError(400, "User not found!"));
             await this.userService.deleteUserById(Number(userId));
             return res.json({ id: userId });
+        } catch (error) {
+            return next(error);
+        }
+    }
+
+    async deleteMySelf(req: IAuthRequest, res: Response, next: NextFunction) {
+        const userId = req.auth.userId;
+        try {
+            const user = await this.userService.findUserById(Number(userId));
+            if (!user) return next(createHttpError(400, "User not found!"));
+
+            await this.userService.deleteUserById(Number(userId));
+
+            res.clearCookie("accessToken");
+            res.clearCookie("refreshToken");
+
+            return res.json({
+                user: null,
+                message: "User deleted successfully",
+            });
+        } catch (error) {
+            return next(error);
+        }
+    }
+
+    async updateFullName(
+        req: IUpdateFullNameRequest,
+        res: Response,
+        next: NextFunction,
+    ) {
+        const result = validationResult(req);
+        if (!result.isEmpty()) {
+            return res.status(400).json({ error: result.array() });
+        }
+
+        const userId = req.auth.userId;
+        const { fullName } = req.body;
+        try {
+            const user = await this.userService.findUserById(Number(userId));
+            if (!user) return next(createHttpError(400, "User not found!"));
+
+            user.fullName = fullName;
+            await this.userService.saveUser(user);
+
+            return res.json({
+                message: "Update user fullName successfully.",
+                user,
+            });
         } catch (error) {
             return next(error);
         }
