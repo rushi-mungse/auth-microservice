62bc4806aa0a56df182dd22bafa54e527a20883b
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2023-12-29 11:10:03 +0530

    add: updateFullNameData validator

--- src/validators/index.ts
+++ src/validators/index.ts
@@ -1,5 +1,6 @@
-export { default as sendOtpDataValidator } from "./sendOtpData.validator";
-export { default as verifyOtpDataValidator } from "./verifyOtpData.validator";
-export { default as loginDataValidator } from "./loginData.validator";
-export { default as forgetPasswordDataValidator } from "./forgetPasswordData.validator";
-export { default as setPasswordDataValidator } from "./setPasswordData.validator";
+export { default as sendOtpDataValidator } from "./auth/sendOtpData.validator";
+export { default as verifyOtpDataValidator } from "./auth/verifyOtpData.validator";
+export { default as loginDataValidator } from "./auth/loginData.validator";
+export { default as forgetPasswordDataValidator } from "./auth/forgetPasswordData.validator";
+export { default as setPasswordDataValidator } from "./auth/setPasswordData.validator";
+export { default as updateFullNameDataValidator } from "./user/updateFullNameData.validator";
