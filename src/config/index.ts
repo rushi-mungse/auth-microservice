a02d872a44f434987096196e03092196e6207c9c
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2023-12-29 15:08:07 +0530

    add: cloudinary env variables

--- src/config/index.ts
+++ src/config/index.ts
@@ -17,7 +17,11 @@ export const {
     PRIVATE_KEY,
     REFRESH_TOKEN_SECRET,
     JWKS_URI,
+    CLOUDINARY_CLOUD_NAME,
+    CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET,
 } = process.env;
 
 export { default as logger } from "./logger";
 export { default as AppDataSource } from "./data-source";
+export { default as uploadOnCloudinary } from "./cloudinary";
