e6ff857594940c713dc5c28f718482433a586d5e
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2023-12-29 09:44:40 +0530

    add: delete user method add

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -7,6 +7,10 @@ class UserController {
 
     async getOne(req: Request, res: Response, next: NextFunction) {
         const userId = req.params.userId;
+        if (isNaN(Number(userId))) {
+            return next(createHttpError(400, "Invalid param id!"));
+        }
+
         try {
             const user = await this.userService.findUserById(Number(userId));
             if (!user) return next(createHttpError(400, "User not found!"));
@@ -20,6 +24,21 @@ class UserController {
         try {
             const users = await this.userService.getAll();
             return res.json({ users });
+        } catch (error) {
+            return next(error);
+        }
+    }
+
+    async delete(req: Request, res: Response, next: NextFunction) {
+        const userId = req.params.userId;
+        if (isNaN(Number(userId)))
+            return next(createHttpError(400, "Invalid param id!"));
+
+        try {
+            const user = await this.userService.findUserById(Number(userId));
+            if (!user) return next(createHttpError(400, "User not found!"));
+            await this.userService.deleteUserById(Number(userId));
+            return res.json({ id: userId });
         } catch (error) {
             return next(error);
         }
