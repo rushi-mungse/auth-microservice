ad751b964ffe9aa6abc9478a650614f81ca7318e
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2023-12-29 08:43:14 +0530

    add: user router

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import "reflect-metadata";
 import express from "express";
 import { errorHandlerMiddleware } from "./middlewares";
-import { authRouter } from "./routes";
+import { authRouter, userRouter } from "./routes";
 import cookieParser from "cookie-parser";
 
 const app = express();
@@ -11,6 +11,7 @@ app.use(express.static("public"));
 app.use(express.json());
 
 app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
 app.use(errorHandlerMiddleware);
 
