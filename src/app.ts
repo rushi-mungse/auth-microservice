496d5e23cca891f5a63bdab9c27b5cd7629c9b4a
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2023-12-30 14:05:45 +0530

    add: cors origin middleware

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,16 @@ import express from "express";
 import { errorHandlerMiddleware } from "./middlewares";
 import { authRouter, userRouter } from "./routes";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 
+const corsOption: cors.CorsOptions = {
+    origin: ["http://localhost:5173"],
+    credentials: true,
+};
+
+app.use(cors(corsOption));
 app.use(cookieParser());
 app.use(express.static("public"));
 app.use(express.json());
